11cab93fbc683f46f5e71553f169298155c7fb94
zakumann <samuel742able@gmail.com>
2024-02-29 18:46:56 +0900

    Old school FPS

---
+++ Source/OldSchoolFPS/OldSchoolFPSGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OldSchoolFPSGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOldSchoolFPSGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOldSchoolFPSGameMode();
+};
+
+
+
